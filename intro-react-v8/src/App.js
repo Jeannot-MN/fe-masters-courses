93fff429c0d56d654b51126768241ed13df15798
JeannotMunganga <jeannotngalula@gmail.com>
2023-05-16 20:21:39 +0200

    Unidirectional data flow Parent -> Child

--- intro-react-v8/src/App.js
+++ intro-react-v8/src/App.js
@@ -1,13 +1,31 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        React.createElement("h1", {}, "Adopt Me!")
-    )
-}
-
-
-console.log("Hello");
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Dodge",
+      animal: "Cat",
+      breed: "General",
+    }),
+    React.createElement(Pet, {
+      name: "John",
+      animal: "Human",
+      breed: "Chinese",
+    }),
+  ]);
+};
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
