d4df7e575a14dd4b831bdfd5dca48c0f47407557
JeannotMunganga <jeannotngalula@gmail.com>
2023-05-18 21:38:03 +0200

    Mapping through list

--- intro-react-v8/src/SearchParams.jsx
+++ intro-react-v8/src/SearchParams.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState();
+  const [breed, setBreed] = useState();
+
+  const breeds = ["JMN"];
 
   return (
     <div className="search-params">
@@ -16,6 +22,48 @@ const SearchParams = () => {
               setLocation(event.target.value);
             }}
           />
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(event) => {
+              setAnimal(event.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((element, index) => {
+              return (
+                <option key={index} value={element}>
+                  {element}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            disabled={!breeds.length}
+            onChange={(event) => {
+              setBreed(event.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((element, index) => {
+              return (
+                <option key={index} value={element}>
+                  {element}
+                </option>
+              );
+            })}
+          </select>
         </label>
 
         <button>Submit</button>
