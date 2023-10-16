d90fa1a5a0f8d6a14ce57c141394c8bb71ceee80
JeannotMunganga <jeannotngalula@gmail.com>
2023-10-16 21:07:12 +0200

    creatin your own compiler -> setup jest

---
+++ own-compiler/src/tokenize.js
@@ -0,0 +1,11 @@
+export function tokenize (input){
+  const tokens = [];
+  let cursor = 0;
+
+  while (cursor < input.length) {
+
+    cursor++;
+  }
+
+  return tokens;
+}
--- own-compiler/src/utils.js
+++ own-compiler/src/utils.js
@@ -3,30 +3,19 @@ const WHITESPACE = /\s+/;
 const NUMBER = /^[0-9]+$/;
 const OPERATORS = ['+', '-', '*', '/', '%'];
 
-const isLetter = character => LETTER.test(character);
+export const isLetter = character => LETTER.test(character);
 
-const isWhitespace = character => WHITESPACE.test(character);
+export const isWhitespace = character => WHITESPACE.test(character);
 
-const isNumber = character => NUMBER.test(character);
+export const isNumber = character => NUMBER.test(character);
 
-const isOpeningParenthesis = character => character === '(';
+export const isOpeningParenthesis = character => character === '(';
 
-const isClosingParenthesis = character => character === ')';
+export const isClosingParenthesis = character => character === ')';
 
-const isParenthesis = character =>
+export const isParenthesis = character =>
   isOpeningParenthesis(character) || isClosingParenthesis(character);
 
-const isQuote = character => character === '"';
+export const isQuote = character => character === '"';
 
-const isOperator = character => OPERATORS.includes(character);
-
-module.exports = {
-  isLetter,
-  isWhitespace,
-  isNumber,
-  isOpeningParenthesis,
-  isClosingParenthesis,
-  isParenthesis,
-  isQuote,
-  isOperator,
-};
+export const isOperator = character => OPERATORS.includes(character);
